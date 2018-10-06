9d9d1443147745850a7bc34d1676ba63d30fe3f9
Vladyslav Pekker <agilesteel@gmail.com>
2018-10-06 19:33:28 +0200

    Video 12 - Stack

---
+++ src/main/scala/homegrown/collections/Stack.scala
@@ -0,0 +1,29 @@
+package homegrown.collections
+
+sealed abstract class Stack[+Element] {
+  import Stack._
+
+  final def `onTopOf:`[Super >: Element](input: Super): Stack[Super] =
+    push(input)
+
+  final def push[Super >: Element](input: Super): Stack[Super] =
+    NonEmpty(input, this)
+
+  final lazy val (peak, pop) = popElement
+
+  final def popElement: (Option[Element], Stack[Element]) =
+    this match {
+      case Empty =>
+        None -> empty
+
+      case NonEmpty(element, otherElements) =>
+        Some(element) -> otherElements
+    }
+}
+
+object Stack {
+  final case class NonEmpty[+Element](element: Element, otherElements: Stack[Element]) extends Stack[Element]
+  final case object Empty extends Stack[Nothing]
+
+  def empty: Stack[Nothing] = Empty
+}
